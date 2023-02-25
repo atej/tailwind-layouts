2c5b85b774bd0a56564d0b23f23f099c9e8681d4
atej <taneja.aseem@gmail.com>
2023-02-26 02:56:24 +0530

    remove netlify ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@ import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import prefetch from '@astrojs/prefetch'
 import mdx from '@astrojs/mdx'
-import netlify from '@astrojs/netlify/edge-functions'
 
 export default defineConfig({
   markdown: {
     syntaxHighlight: 'prism',
   },
   integrations: [tailwind(), react(), prefetch(), mdx()],
-  output: 'server',
-  adapter: netlify(),
 })
