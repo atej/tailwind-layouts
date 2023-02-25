ab4dc96af63b5419aa906101d57c0bdd87f39d32
atej <taneja.aseem@gmail.com>
2023-02-26 01:14:50 +0530

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import prefetch from '@astrojs/prefetch'
 import mdx from '@astrojs/mdx'
+import netlify from '@astrojs/netlify/edge-functions'
 
 export default defineConfig({
   markdown: {
     syntaxHighlight: 'prism',
   },
   integrations: [tailwind(), react(), prefetch(), mdx()],
+  output: 'server',
+  adapter: netlify(),
 })
