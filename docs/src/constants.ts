fa7fede5bcb8499e7fccc899450ed989e02dda2f
atej <taneja.aseem@gmail.com>
2023-02-26 11:23:04 +0530

    update file  links

--- docs/src/constants.ts
+++ docs/src/constants.ts
@@ -3,8 +3,8 @@ import type { NavLinks, PathName } from './types'
 export const SITE_TITLE = 'Tailwind Layouts'
 export const SITE_DESCRIPTION = 'Layout primitives for TailwindCSS.'
 export const GITHUB_URL = 'https://github.com/atej/tailwind-layouts'
-export const DEFAULT_THEME_URL = `${GITHUB_URL}/blob/main/plugin/lib/theme.ts`
-export const DEFAULT_OPTIONS_URL = `${GITHUB_URL}/blob/main/plugin/lib/options.ts`
+export const DEFAULT_THEME_URL = `${GITHUB_URL}/blob/main/tailwind-layouts/lib/theme.ts`
+export const DEFAULT_OPTIONS_URL = `${GITHUB_URL}//blob/main/tailwind-layouts/lib/options.ts`
 export const AUTHOR_TWITTER_URL = 'https://twitter.com/aseemtaneja'
 
 export const PRIMARY_LINKS: NavLinks<PathName> = [
