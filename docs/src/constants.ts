7b5feaf13e946e19ab2afadbeaa1ce6f2e575187
atej <taneja.aseem@gmail.com>
2023-02-27 12:23:04 +0530

    make tailwind css two words

--- docs/src/constants.ts
+++ docs/src/constants.ts
@@ -1,7 +1,7 @@
 import type { NavLinks, PathName } from './types'
 
 export const SITE_TITLE = 'Tailwind Layouts'
-export const SITE_DESCRIPTION = 'Layout primitives for TailwindCSS.'
+export const SITE_DESCRIPTION = 'Layout primitives for Tailwind CSS.'
 export const GITHUB_URL = 'https://github.com/atej/tailwind-layouts'
 export const DEFAULT_THEME_URL = `${GITHUB_URL}/blob/main/tailwind-layouts/lib/theme.ts`
 export const DEFAULT_OPTIONS_URL = `${GITHUB_URL}//blob/main/tailwind-layouts/lib/options.ts`
