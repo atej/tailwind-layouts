778d37091d9372dfbed7fbf8604256d490047682
atej <taneja.aseem@gmail.com>
2023-02-27 13:01:32 +0530

    add twiiter meta

--- docs/src/constants.ts
+++ docs/src/constants.ts
@@ -5,7 +5,7 @@ export const SITE_DESCRIPTION = 'Layout primitives for Tailwind CSS.'
 export const GITHUB_URL = 'https://github.com/atej/tailwind-layouts'
 export const DEFAULT_THEME_URL = `${GITHUB_URL}/blob/main/tailwind-layouts/lib/theme.ts`
 export const DEFAULT_OPTIONS_URL = `${GITHUB_URL}//blob/main/tailwind-layouts/lib/options.ts`
-export const AUTHOR_TWITTER_URL = 'https://twitter.com/aseemtaneja'
+export const AUTHOR_TWITTER = 'aseemtaneja'
 
 export const PRIMARY_LINKS: NavLinks<PathName> = [
   { slug: 'usage', text: 'Usage' },
