22a77060a952ef60e41133532dc2b8655a16be07
atej <taneja.aseem@gmail.com>
2023-03-21 12:40:48 +0530

    apply flexbox context only if split is required

--- tailwind-layouts/lib/utilities/layouts/stack.ts
+++ tailwind-layouts/lib/utilities/layouts/stack.ts
@@ -12,9 +12,6 @@ const stack: Utility = {
     ({ options, theme }) => [
       {
         [`.${c(options, 'stack')}`]: {
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
           '& > *': {
             [l(options, 'marginTop')]: '0',
             [l(options, 'marginBottom')]: '0',
@@ -42,6 +39,11 @@ const stack: Utility = {
     ({ options, theme }) => [
       {
         [`${c(options, 'stack')}_split-after`]: (value) => ({
+          [`&.${c(options, 'stack')}`]: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+          },
           [`&.${c(options, 'stack')}>:nth-child(${value})`]: {
             [l(options, 'marginBottom')]: 'auto',
           },
